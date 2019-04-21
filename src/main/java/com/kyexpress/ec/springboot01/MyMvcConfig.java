381a0207a9e2b65511e59b7c7c5fc21cde89e54b
xjq946 <xjq111006@163.com>
2019-04-21 22:38:41 +0800

    更新代码

--- src/main/java/com/kyexpress/ec/springboot01/MyMvcConfig.java
+++ src/main/java/com/kyexpress/ec/springboot01/MyMvcConfig.java
@@ -1,11 +1,10 @@
 package com.kyexpress.ec.springboot01;
 
+import com.kyexpress.ec.springboot01.controller.component.LoginHandlerInterceptor;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 
 @Configuration
@@ -16,6 +15,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/atguigu").setViewName("success");
     }
 
+
     @Bean
     public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
         WebMvcConfigurerAdapter webMvcConfigurerAdapter=new WebMvcConfigurerAdapter() {
@@ -23,8 +23,30 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //不要使用/**，否则静态资源还是会被拦截
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login","/static/**");
+            }
+
+            //对静态资源进行放行
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**")
+                        .addResourceLocations("classpath:/static/");
+                super.addResourceHandlers(registry);
             }
         };
         return webMvcConfigurerAdapter;
+
     }
+
+
+
 }
